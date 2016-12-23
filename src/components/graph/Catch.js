79ea2e94920de03e66ff790f389d08b0dccd1427
Lea Fox <lsfox91@gmail.com>
2016-12-22 16:55:34 -0800

    r-router & express can communicate

--- src/components/graph/Catch.js
+++ src/components/graph/Catch.js
@@ -3,33 +3,20 @@ import NavLink from '../NavLink';
 import { browserHistory } from 'react-router';
 import request from 'request';
 
+
 export default class Catch extends React.Component{
 
   componentDidMount() {
-     //Asynchronous ish!
-    console.log("In cdm of Catch");
-    var postInputUrl = new XMLHttpRequest();
-    console.log(JSON.stringify(postInputUrl, null, 2));
-    postInputUrl.onerror = function(err){ console.log(err);}
-    postInputUrl.onloadend = this.handlePostRes(postInputUrl);
-    postInputUrl.open("POST", "/catch", true);
-    postInputUrl.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    postInputUrl.send("url="+this.props.url);
-
-    let now = new Date();
-    console.log(`Home updated @ ${now.toUTCString()}`);
-
+    //Asynchronous ish!
+    request.post('http://localhost:3000/catch', {form: {url:this.props.url}},
+      function (err, res, body){
+        if(err)console.log(err);
+        browserHistory.push('graph')
+        console.log('Upload successful!  Server responded with:', body);
+    });
   }
-  handlePostRes(postInputUrl){
-    //Call a function when the state changes.
-    console.log("handlePostRes is running", postInputUrl.readyState);
-    if(postInputUrl.status == 200) {
-        // Request finished. Do processing here.
-        console.log("Post Response Successful", postInputUrl.response);
-        browserHistory.push('graph');
-    }
 
-  }
+
   cleanURL(urlStr){
     return urlStr;
   }

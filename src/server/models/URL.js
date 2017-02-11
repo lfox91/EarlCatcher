2806d1af18224e1bea27a1d8fa2e97b286abff26
Lea Fox <lsfox91@gmail.com>
2017-02-10 17:01:50 -0800

    almost

--- src/server/models/URL.js
+++ src/server/models/URL.js
@@ -1,10 +1,10 @@
-var request = require('request');
+var rp = require('request-promise');
 var cheerio = require('cheerio');
 var Promise = require("bluebird");
 
-class Node {
-  constructor(name, ...props){
-    this.edges = new Set();
+class LinkNode {
+  constructor(name, edges, ...props){
+    this.edges = new Set(edges);
     this.name = name;
     this.props = props;
   }
@@ -22,8 +22,8 @@ class Graph {
     this.nodes = {};
     this.name = name;
   }
-  addNode(name, props){
-    this.nodes[name] = new Node(name, props);
+  addNode(name,edges, props){
+    this.nodes[name] = new LinkNode(name,edges, props);
   }
   addEdge(name, end){
     // console.log("In addEdge: ", this.nodes[name]);
@@ -35,90 +35,72 @@ class Graph {
 }
 
 class URL extends Graph {
-  constructor(name){
-    super(name)
-    this.name = name;
+  constructor(root){
+    super(root)
+    this.root = root;
     this.visited = {};
     this.count = 0;
   }
   //Need to track eachLink visited
-  add(name, props){
-    this.addNode(name, props);
+  add(name, edges, props){
+    this.addNode(name, edges, props);
     this.visited[name] = true;
   }
-  addLink(name, end){
-  //  console.log("Add Edge", this.addEdge);
-    this.addEdge(name, end)
-    // console.log(this.nodes[name].edges);
-  }
-  getLinks(url, text, href){
-    var promises = [];  //req.body.url
-    let linkPromise = new Promise((resolve, reject) => {
 
-      if(this.visited[url] || !url || url[0] !== '/' && url !== this.name ) {
-        // console.log("BREAK RECURSION", url);
-        return;
-      } else{
-
-        request.get(url, (error, response, body) => {
-          if (!error && response.statusCode == 200) {
-            let $ = cheerio.load(body);
-
-            /* $('a').each(function(i, el){});*/
-            var len = $('a').length
-            // console.log("LENGTH:   "+len);
-            if(!this.visited[this.name]){
-              this.add(this.name);
-            }
-            console.log(url, this.visited[url]);
-            for( var i = 0; i<len; i++){
-              var el = $('a').eq(i);
-              var href =  el.attr('href');
-              // console.log(i, href);
-              if(!href || href[0] !== '/' )continue;
-              href = "http://seaver.pepperdine.edu" + href;
-              var info = {text: el.text(), href: href };
-
-              if(!this.visited[info.href] && info.href !== this.name){
-
-                // console.log("visited check",href, this.visited[info.href]);
-                // console.log(`${i}: ${info.href}`);
-                this.add(info.href, [info.text, info.href])
-
-                ++this.count;
-
-                this.addLink(url, info.href);
-
-                console.log(info.href);
-                this.getLinks(info.href)
-
-                // console.log(this.nodes[info.href]);
-
-              }
-            }
-              resolve(this);
-          } else {
-            reject(error);
-          }
-        })
+  getLinks(root, props){ // props = text, href
+    var that = this;
+    if(that.visited[root] != true && (root.length ||  that.root == root)){
+      root =  typeof root == 'string'? root : that.root + $(root).attr('href');
+      let options = {
+        uri: root,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
       }
-    })
-    promises.push(linkPromise.bind(this));
-    return Promise.all(promises);
-  }
-  generateGraph(url){
-    // should run after root has been added  - might change that?
+      rp(options)
+        .then(($)=>{
+          var anchorArr = [];
+          var tempEdges = [];
 
-    this.getLinks(url).then(function(result){
-      //This should be the completed graph of site
-      console.log(JSON.stringify(result, null, 1));
-    }, function(err){throw(err)}).bind(this);
+          $('a').each((i, el) => {
+            tempEdges[i] = el.text;
+            anchorArr[i] = el; // ccheck iinput
+          });
+          that.add(root, anchorArr, props);
+          ++that.count;
+          console.log(that.count, root);
+          that.nodes[root].edges.forEach(function(anchorObj){
+            var href = that.root + $(anchorObj).attr('href');
+            if(!this.visited[href]){
+              return that.getLinks(href,[anchorObj.text, href]);
+            }
+          })
+        })
+        .catch((err) => {
+          //console.log(err);
+          that.add(root, [err]);
+        })
+        return that;
+    }
+
   }
+
+  // static scrapeForLinks(){
+  //
+  // }
+  // generateGraph(root){
+  //   // should run after root has been added  - might change that?
+  //
+  //   this.getLinks(root).then(function(result){
+  //     //This should be the completed graph of site
+  //     console.log(JSON.stringify(result, null, 1));
+  //   }, function(err){throw(err)}).bind(this);
+  // }
 }
 
 
 var root = new URL("http://seaver.pepperdine.edu");
-root.generateGraph("http://seaver.pepperdine.edu");
+ root.getLinks(root.root);
 // console.log("Line 109", root)
 
 module.exports = URL;
@@ -167,3 +149,58 @@ module.exports = URL;
 //
 //
 // }
+/*
+var promises = [];  //req.body.url
+let linkPromise = new Promise((resolve, reject) => {
+
+  if(this.visited[url] || !url || url[0] !== '/' && url !== this.name ) {
+    // console.log("BREAK RECURSION", url);
+    return;
+  } else{
+
+    request.get(url, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        let $ = cheerio.load(body);
+
+         //$('a').each(function(i, el){});
+        var len = $('a').length
+        // console.log("LENGTH:   "+len);
+        if(!this.visited[this.name]){
+          this.add(this.name);
+        }
+        console.log(url, this.visited[url]);
+        for( var i = 0; i<len; i++){
+          var el = $('a').eq(i);
+          var href =  el.attr('href');
+          // console.log(i, href);
+          if(!href || href[0] !== '/' )continue;
+          href = "http://seaver.pepperdine.edu" + href;
+          var info = {text: el.text(), href: href };
+
+          if(!this.visited[info.href] && info.href !== this.name){
+
+            // console.log("visited check",href, this.visited[info.href]);
+            // console.log(`${i}: ${info.href}`);
+            this.add(info.href, [info.text, info.href])
+
+            ++this.count;
+
+            this.addEdge(url, info.href);
+
+            console.log(info.href);
+            this.getLinks(info.href)
+
+            // console.log(this.nodes[info.href]);
+
+          }
+        }
+          resolve(this);
+      } else {
+        reject(error);
+      }
+    })
+  }
+})
+promises.push(linkPromise.bind(this));
+return Promise.all(promises);
+ */

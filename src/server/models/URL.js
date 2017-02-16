4194f1b190755704df624224b5f90350fd9d184a
Lea Fox <lsfox91@gmail.com>
2017-02-15 16:58:42 -0800

    recursive promise fixing

--- src/server/models/URL.js
+++ src/server/models/URL.js
@@ -1,7 +1,12 @@
 var rp = require('request-promise');
 var cheerio = require('cheerio');
 var Promise = require("bluebird");
+require('cute-stack')('pretty', Infinity);
+var longjohn = require('longjohn');
+var debug = require('debug');
+var Promise = require('bluebird');
 
+longjohn.async_trace_limit = 5;
 class LinkNode {
   constructor(name, edges, ...props){
     this.edges = new Set(edges);
@@ -45,160 +50,76 @@ class URL extends Graph {
     this.visited[name] = true;
     ++this.count;
     console.log(this.count, name);
-    if(this.count>2183){ throw new Error("YOUR GONNA BREAK SOMETHING OVER 2183!!!")}
+    if(this.count>3000){ throw new Error("YOUR GONNA BREAK SOMETHING OVER 2183!!!")}
+    this.stack
   }
 
   getLinks(root, props){ // props = text, href
-    var that = this;
-    if(that.visited[root] !== true && root.length){
-      // && root[0] == '/' || that.root == root
+    Error.captureStackTrace(this)
+    if(this.visited[root] != true && root.length){
+      console.log("Check after 1st IF ==", this.visited[root]);
       let options = {
         uri: root,
+        timeout: 5000,
         transform: function (body) {
             return cheerio.load(body);
-        }
+        },
+        transform2xxOnly: false,
+        gzip: true
       }
-       rp(options)
+       rp(options).promise().bind(this)
         .then(($)=>{
           var tempEdges = [];
           $('a').each((i, el) => {
             let href = $(el).attr('href');
             if(href) tempEdges[i] = href;
           });
-          that.add(root, tempEdges, props);
+          function b (data, cb){
+            data = setTimeout((data)=>{return data+2}, 50, 3)
+            cb(null, data);
+          }
+          return Promise.promisify(b);
 
-          that.nodes[root].edges.forEach(function(uri){
-            if(that.visited[that.root+uri] !== true && uri[0] == "/"){
-              setTimeout(that.getLinks, 5000, that.root+uri,["text placeholder", uri]);
-              return that;
-            }
-          })
-        }).then((url)=>{
-          console.log(url);
+          // console.log("Check before add ==", this.visited[root]);
+          // this.add(root, tempEdges, props);
+          // this.nodes[root].edges.forEach((uri)=>{
+          //   if(uri.indexOf("#")!==-1){
+          //     uri = uri.slice(0, uri.indexOf("#"));
+          //   }
+          //   if(uri[uri.length-1]=="/"){
+          //     uri = uri.slice(0, uri.length-1);
+          //   }
+          //
+          //   if(this.visited[this.root+uri] !== true && uri[0] == "/"){
+          //     console.log(this.stack )
+          //     setTimeout(this.getLinks.bind(this), 5000, this.root+uri,["text placeholder", uri]);
+          //     return this;
+          //   }
+          // })
+        })
+        .then((something)=>{
+
+          something.then(
+            (data)=>console.log(data)
+          )
+
         }, (err)=>{
-          console.error(err);
+          console.log(err);
+          throw err;
         })
         .catch((err) => {
-          console.error(err);
-          that.add(root, [err]);
+          if(err.cause && err.options && err.cause.code == "ETIMEDOUT" || "ESOCKETTIMEDOUT"){
+            console.error(JSON.stringify(err, null, ">>>"));
+            //this.add(root, [err.cause.code, err.options.uri]);
+          } else {
+            console.error(JSON.stringify(err, null, "###"));
+            //this.add(root, [err]);
+          }
         })
     }
   }
-
-  // static scrapeForLinks(){
-  //
-  // }
-  // generateGraph(root){
-  //   // should run after root has been added  - might change that?
-  //
-  //   this.getLinks(root).then(function(result){
-  //     //This should be the completed graph of site
-  //     console.log(JSON.stringify(result, null, 1));
-  //   }, function(err){throw(err)}).bind(this);
-  // }
 }
 
-
-var root = new URL("http://seaver.pepperdine.edu");
+var root = new URL("http://www.seaver.pepperdine.edu");
 var seaver = root.getLinks(root.root);
 module.exports = URL;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// NOTE: Memoize URLs to speed up decision making
-// root.add("http://seaver.pepperdine.edu", [{some:"stuff"}, {more:"stuff"}]);
-// root.add("http://www.pepperdine.edu", [{some:"stuff"}, {more:"stuff"}]);
-// root.add("http://community.pepperdine.edu", [{some:"stuff"}, {more:"stuff"}]);
-// root.addLink("http://seaver.pepperdine.edu", "http://seaver.pepperdine.edu/admission");
-// root.addLink("http://seaver.pepperdine.edu", "http://seaver.pepperdine.edu/about");
-// root.addLink("http://seaver.pepperdine.edu", "http://seaver.pepperdine.edu/academics");
-// root.addLink("http://seaver.pepperdine.edu", "http://seaver.pepperdine.edu/donate");
-// console.log("ROOT: ", root);
-// console.log("JSONS ROOT: " + JSON.stringify(root, " ", 1));
-// console.log("EDGE ", root.nodes["http://seaver.pepperdine.edu"].edges);
-// console.log("JSONS EDGE" + JSON.stringify(root.nodes["http://seaver.pepperdine.edu"].edges));
-//  res.send({links:links})
-//One function that goes to links and makes an array of new links
-// newgetLinks(url){ get All Links
-//     request.get(url, function(error, response, body){
-//       if(err) throw err;
-//       if(!error){
-//         let $ = cheerio.load(body);
-//         var count = 0;
-//         return Array.from($('a'));
-//         console.log(linkArray[0]);
-//       }
-//   });
-// }
-//
-//One function that loops through link array and updates graph
-// getArrLink(arr){
-//   let get = Promise.promisify(this.newgetLinks);
-//
-//
-// }
-/*
-var promises = [];  //req.body.url
-let linkPromise = new Promise((resolve, reject) => {
-
-  if(this.visited[url] || !url || url[0] !== '/' && url !== this.name ) {
-    // console.log("BREAK RECURSION", url);
-    return;
-  } else{
-
-    request.get(url, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        let $ = cheerio.load(body);
-
-         //$('a').each(function(i, el){});
-        var len = $('a').length
-        // console.log("LENGTH:   "+len);
-        if(!this.visited[this.name]){
-          this.add(this.name);
-        }
-        console.log(url, this.visited[url]);
-        for( var i = 0; i<len; i++){
-          var el = $('a').eq(i);
-          var href =  el.attr('href');
-          // console.log(i, href);
-          if(!href || href[0] !== '/' )continue;
-          href = "http://seaver.pepperdine.edu" + href;
-          var info = {text: el.text(), href: href };
-
-          if(!this.visited[info.href] && info.href !== this.name){
-
-            // console.log("visited check",href, this.visited[info.href]);
-            // console.log(`${i}: ${info.href}`);
-            this.add(info.href, [info.text, info.href])
-
-            ++this.count;
-
-            this.addEdge(url, info.href);
-
-            console.log(info.href);
-            this.getLinks(info.href)
-
-            // console.log(this.nodes[info.href]);
-
-          }
-        }
-          resolve(this);
-      } else {
-        reject(error);
-      }
-    })
-  }
-})
-promises.push(linkPromise.bind(this));
-return Promise.all(promises);
- */
